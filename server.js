a92f7bb4e3782b0eb3172b9721ecc05fafb81a82
wingsvn <wing-san.lee@live.nl>
2024-02-29 14:30:26 +0100

    V5 POST data (API), img fallback

--- server.js
+++ server.js
@@ -67,7 +67,6 @@ app.get('/', function (request, response) {
       squads:squadData.data
     })
   })
-  
 })
 
 // Maak een POST route voor de index
@@ -79,28 +78,80 @@ app.post('/', function (request, response) {
   response.redirect(303, '/')
 })
 
-  
+
+///////// DETAILPAGINA + POST-data 
 
 // Maak een GET route voor een detailpagina met een request parameter id
 app.get('/person/:id', function (request, response) {
   // Gebruik de request parameter id en haal de juiste persoon uit de WHOIS API op
   fetchJson(apiUrl + '/person/' + request.params.id).then((apiData) => {
-    // Render person.ejs uit de views map en geef de opgehaalde data mee als variable, genaamd person
+    // messages:messages
+    console.log(messages) 
+    // Het custom field is een String, dus die moeten we eerst omzetten (= parsen)
+    // naar een Object, zodat we er mee kunnen werken
+    try {
+      apiData.data.custom = JSON.parse(apiData.data.custom)
+    } catch (error) {}
+    
+    // Render person.ejs uit de views map en geef de opgehaalde data mee als variable, genaamd person 
     response.render('person', {
-      person: apiData.data, 
+      person:apiData.data, 
       squads:squadData.data, 
       messages:messages
     })
   })
-
-  // Maak een POST route voor de index
-app.post('/', function (request, response) {
-  //voeg een nieuw bericht toe aan de messages array
-  messages.push(request.body.bericht)
-
-  // Er is nog geen afhandeling van POST, redirect naar GET op /
-  response.redirect(303, '/')
 })
+
+// Als we vanuit de browser een POST doen op de detailpagina van een persoon
+app.post('/person/:id', function(request, response) {
+
+  // Stap 1: Haal de huidige data op, zodat we altijd up-to-date zijn, en niks weggooien van anderen
+
+  // Haal eerst de huidige gegevens voor deze persoon op, uit de WHOIS API
+  fetchJson('https://fdnd.directus.app/items/person/' + request.params.id).then((apiResponse) => {
+
+    // Het custom field is een String, dus die moeten we eerst
+    // omzetten (= parsen) naar een Object, zodat we er mee kunnen werken
+    try {
+      apiResponse.data.custom = JSON.parse(apiResponse.data.custom)
+    } catch (e) {
+      apiResponse.data.custom = {}
+    }
+
+
+  // Stap 2: Gebruik de data uit het formulier
+    // Deze stap zal voor iedereen net even anders zijn, afhankelijk van de functionaliteit
+
+    // Controleer eerst welke actie is uitgevoerd, aan de hand van de submit button
+    // Dit kan ook op andere manieren, of in een andere POST route
+    if (request.body.actie == 'verstuur') {
+
+      // Als het custom object nog geen messages Array als eigenschap heeft, voeg deze dan toe
+      if (!apiResponse.data.custom.messages) {
+        apiResponse.data.custom.messages = []
+      }
+
+      // Voeg een nieuwe message toe voor deze persoon, aan de hand van het bericht uit het formulier
+      apiResponse.data.custom.messages.push(request.body.message)
+    }
+
+  // Stap 3: Sla de data op in de API
+
+    // Voeg de nieuwe lijst messages toe in de WHOIS API,
+    // via een PATCH request
+    fetch('https://fdnd.directus.app/items/person/' + request.params.id, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        custom: apiResponse.data.custom
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    }).then((patchResponse) => {
+      // Redirect naar de persoon pagina
+      response.redirect(303, '/person/' + request.params.id)
+    })
+  })
 })
 
 
